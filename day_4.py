b1d2f32a908207a645a2faf20be381b28cdec27f
Meggin <megginsimon@gmail.com>
2022-12-05 05:40:21 +0000

    solved day 4

---
+++ day_4.py
@@ -0,0 +1,19 @@
+import re
+
+def find_overlapping_sections(sections):
+
+    data = open(sections)
+
+    fully_contained_pairs = 0
+
+    for line in data:
+        info = line.rstrip()
+        elf_a_start, elf_a_finish, elf_b_start, elf_b_finish = re.split("-|,", info)
+        
+        if int(elf_a_start) <= int(elf_b_start) and int(elf_a_finish) >= int(elf_b_finish):
+            fully_contained_pairs += 1
+        elif int(elf_a_start) >= int(elf_b_start) and int(elf_a_finish) <= int(elf_b_finish):
+            fully_contained_pairs += 1 
+
+    print(fully_contained_pairs)
+
