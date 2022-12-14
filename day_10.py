80ea5d55e72f7a547d62c3bff38f07f8f3c52fd9
Meggin <megginsimon@gmail.com>
2022-12-14 17:37:16 +0000

    day10 pt one complete

---
+++ day_10.py
@@ -0,0 +1,20 @@
+instructions = open("inputs/10input.txt").read().split()
+
+x = i = 1
+totals = []
+signal_strength =0
+cycle = 20
+
+for instruction in instructions:
+    if instruction != "addx" and instruction != "noop":
+        x += int(instruction)
+    i+=1
+
+    if i % cycle == 0 and i != 0.0:
+        totals.append(i*x)
+        cycle += 40
+
+for total in totals:
+    signal_strength += total
+
+print(signal_strength)
